539e8b658a0dac152369ed8b13d740a20aedbaf3
MING IT <mingit55@gmail.com>
2024-09-14 10:16:27 +0900

    Fix: 커넥션 끊기는 버그 수정

--- src/db.js
+++ src/db.js
@@ -4,11 +4,29 @@ const mysql = require("mysql2/promise");
 module.exports = {
   async init() {
     // Create the connection to database
-    global._db = await mysql.createConnection({
+    global._dbpool = mysql.createPool({
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
-    });
+      connectionLimit: 10, // Maximum number of connections
+      waitForConnections: true, // Wait for a connection before creating a new one
+      // maxWaitTime: 5000, // Maximum time to wait for a connection in milliseconds
+    })
+
+    global._db = {
+      async query() {
+        const conn = await  _dbpool.getConnection();
+        const result = conn.query(...arguments);
+        conn.release();
+        return result;
+      },
+      async execute() {
+        const conn = await  _dbpool.getConnection();
+        const result = conn.execute(...arguments);
+        conn.release();
+        return result;
+      },
+    };
   },
 };
